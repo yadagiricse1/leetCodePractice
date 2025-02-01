abba632a5436a704b56cc88ced3e389ec1c3a16d
yadagirimallipeddi <yadagiri@kunaico.com>
2025-01-31 23:49:11 -0600

    Jan 31st commit

---
+++ src/main/java/practice/LinkedListCycle.java
@@ -0,0 +1,19 @@
+package practice;
+
+public class LinkedListCycle {
+    public static boolean hasCycle(ListNode head) {
+        if(head==null)
+            return false;
+        ListNode fast=head;
+        ListNode slow=head;
+        while(fast!=null&&fast.next!=null){
+            fast=fast.next.next;
+            slow=slow.next;
+            if(fast==slow)
+                return true;
+
+        }
+
+        return false;
+    }
+}
---
+++ src/main/java/practice/ListNode.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class ListNode {
+    int val;
+    ListNode next;
+     ListNode(int x) {
+         val = x;
+          next = null;
+      }
+}
